7b0d90c98b5847f6c761cc194bfae939f2b78653
Clinton Morrison <contact@clintonmorrison.com>
2019-05-19 22:44:52 -0400

    add built version

---
+++ frontend/build/precache-manifest.559606df602b9d92527a1bebfbc3b3e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8a2990a3c4b74f23e7973c2e6007fba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e030749d8d06520fd0e9",
+    "url": "/static/css/main.8034f134.chunk.css"
+  },
+  {
+    "revision": "f2fee2cc5b1393a476fe",
+    "url": "/static/js/2.b952c306.chunk.js"
+  },
+  {
+    "revision": "e030749d8d06520fd0e9",
+    "url": "/static/js/main.c0c906e7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
