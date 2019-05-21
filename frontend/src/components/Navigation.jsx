cc89f7ee0be6ee8770c146f78190ecc42a1a8bdc
Clinton Morrison <contact@clintonmorrison.com>
2019-05-20 22:05:46 -0400

    add mobile nav

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -2,22 +2,61 @@ import React from 'react';
 import { withRouter, Link } from 'react-router-dom';
 
 import './Navigation.scss';
+
+const loggedOutItems = [
+  <li key="register"><Link to="/register">Register</Link></li>,
+  <li key="login"><Link to="/login">Login</Link></li>,
+];
+
+const loggedInItems = [
+  <li key="account"><Link to="/account">My Account</Link></li>,
+  <li key="passwords"><Link to="/passwords">My Passwords</Link></li>,
+  <li key="logout"><Link to="/logout">Logout</Link></li>
+];
+
+
 export class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.sidebarRef = React.createRef();
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      window.M.Sidenav.init(this.sidebarRef.current);
+    }, 0);
+  }
+
+  componentDidUpdate() {
+    window.M.Sidenav.getInstance(this.sidebarRef.current).close();
+  }
 
   render() {
     const { services } = this.props;
     const loggedIn = services.authService.sessionExists();
 
-    const loggedOutItems = [
-      <li key="register"><Link to="/register">Register</Link></li>,
-      <li key="login"><Link to="/login">Login</Link></li>,
-    ];
+    const items = loggedIn ? loggedInItems : loggedOutItems;
 
-    const loggedInItems = [
-      <li key="account"><Link to="/account">My Account</Link></li>,
-      <li key="passwords"><Link to="/passwords">My Passwords</Link></li>,
-      <li key="logout"><Link to="/logout">Logout</Link></li>
-    ];
+    return (
+      <div className="cp-navigation">
+        <nav>
+          <div className="nav-wrapper">
+            <Link to="/" className="brand-logo">Lorikeet</Link>
+            <a href="#" data-target="mobile-demo" className="sidenav-trigger right hide-on-med-and-up">
+              <i className="material-icons">menu</i>
+            </a>
+            <ul className="right hide-on-small-and-down">
+              {items}
+            </ul>
+          </div>
+        </nav>
+
+        <ul className="sidenav" id="mobile-demo" ref={this.sidebarRef}>
+          {items}
+        </ul>
+      </div>
+  );
+
 
     return (
       <nav className="">
