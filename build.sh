b3c2a06243183ea8fa6c2f84a231b75467fd86d0
Clinton Morrison <contact@clintonmorrison.com>
2019-06-02 02:32:56 -0400

    build script

--- build.sh
+++ build.sh
@@ -1,3 +1,1 @@
-go build -o server cmd/server/main.go
-go build -o doBackup cmd/backup/main.go
-go build -o doRestore cmd/restore/main.go
+git pull && go build -o server cmd/server/main.go && go build -o doBackup cmd/backup/main.go && go build -o doRestore cmd/restore/main.go
