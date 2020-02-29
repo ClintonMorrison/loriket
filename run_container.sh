9c0b45b78438359cbad686c959ed406a0803e3c3
Clinton Morrison <contact@clintonmorrison.com>
2020-02-29 16:13:25 -0500

    change to variable

--- run_container.sh
+++ run_container.sh
@@ -2,13 +2,12 @@
 
 source version.sh
 
-PROJECT_PATH="$HOME/go/src/github.com/ClintonMorrison/lorikeet"
 CONTAINER_PATH="/opt/go/src/github.com/ClintonMorrison/lorikeet"
 
 docker run -d \
   --name lorikeet \
   -p "8020:80" \
-  -v "$PROJECT_PATH/data:$CONTAINER_PATH/data" \
-  -v "$PROJECT_PATH/log:$CONTAINER_PATH/log" \
-  -v "$PROJECT_PATH/backup:$CONTAINER_PATH/backup" \
+  -v "$LORIKEET_PATH/data:$CONTAINER_PATH/data" \
+  -v "$LORIKEET_PATH/log:$CONTAINER_PATH/log" \
+  -v "$LORIKEET_PATH/backup:$CONTAINER_PATH/backup" \
   "clintonmorrison/projects:lorikeet-$VERSION"
