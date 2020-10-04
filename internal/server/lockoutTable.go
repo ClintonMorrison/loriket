506f3df8cb5ec8e512ae64ccf221f777ac3cd964
Clinton Morrison <contact@clintonmorrison.com>
2020-10-03 22:58:21 -0400

    fix bugs with lockout table, add locking

--- internal/server/lockoutTable.go
+++ internal/server/lockoutTable.go
@@ -1,26 +1,33 @@
 package server
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 const errorWindow = time.Hour * 5
 const maxErrorsInWindow = 5
 
 type LockoutTable struct {
-	errorTimesByIP map[string][]time.Time
+	errorTimesByIP   map[string][]time.Time
 	errorTimesByUser map[string][]time.Time
+	mux              sync.RWMutex
 }
 
 func NewLockoutTable() *LockoutTable {
 	return &LockoutTable{
-		errorTimesByIP: make(map[string][]time.Time, 0),
+		errorTimesByIP:   make(map[string][]time.Time, 0),
 		errorTimesByUser: make(map[string][]time.Time, 0)}
 }
 
 func (l *LockoutTable) shouldAllow(ip string, username string) bool {
 	l.purgeErrors(ip, username)
 
+	l.mux.RLock()
+	defer l.mux.RUnlock()
+
 	byIP := l.errorTimesByIP[ip]
-	byUser := l.errorTimesByIP[username]
+	byUser := l.errorTimesByUser[username]
 
 	if len(byIP) > maxErrorsInWindow || len(byUser) > maxErrorsInWindow {
 		return false
@@ -32,17 +39,23 @@ func (l *LockoutTable) shouldAllow(ip string, username string) bool {
 func (l *LockoutTable) logFailure(ip string, username string) {
 	t := time.Now()
 
+	l.mux.Lock()
+	defer l.mux.Unlock()
+
 	if len(ip) > 0 {
 		l.errorTimesByIP[ip] = append(l.errorTimesByIP[ip], t)
 	}
 
 	if len(username) > 0 {
-		l.errorTimesByUser[username] = append(l.errorTimesByIP[username], t)
+		l.errorTimesByUser[username] = append(l.errorTimesByUser[username], t)
 	}
 }
 
 func (l *LockoutTable) purgeErrors(ip string, username string) {
 	earlistTime := time.Now().Add(errorWindow * -1)
+
+	l.mux.Lock()
+	defer l.mux.Unlock()
 
 	// Errors by IP
 	ipErrorTimes := l.errorTimesByIP[ip]
